54ec74251ad663d9c43f222f67250ddb0e569fa0
Carlos Hernani <chb.hernani@outlook.com>
2017-02-27 23:15:36 -0300

    Server adapted to execute on Azure Functions

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const http = require('http');
+const mock = require('./azure_mock');
+const lastFM = require('./middleware/last_fm');
 
+const hostname = '127.0.0.1';
 const port = 3000;
 
-const requestHandler = (request, response) => {
-  console.log(request.url);
-  response.end('Hello Node.js Server!');
-};
-
-const server = http.createServer(requestHandler);
-
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err);
-  }
-
-  return console.log(`server is listening on ${port}`);
+http.createServer((req, res) => {
+  lastFM(mock.parseContext(res), mock.parseRequest(req));
+}).listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
---
+++ middleware/last_fm.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+module.exports = (context, req) => {
+  const key = req.params.key;
+  const artist = req.params.artist;
+
+  const basePath = 'http://ws.audioscrobbler.com/2.0/';
+  const url = `${basePath}?method=artist.getinfo&artist=${artist}&api_key=${key}&format=json`;
+
+  function errorHandler(error) {
+    context.log(error);
+    context.done(null, {
+      status: error.response.status || 502,
+      body: {
+        message: 'Some error happened',
+        result: {
+          status: error.response.status,
+          data: error.response.data,
+        },
+      },
+    });
+  }
+
+  function responseHandler(response) {
+    context.done(null, {
+      status: 200,
+      body: response.data,
+    });
+  }
+
+  axios.get(url)
+    .then(response => responseHandler(response))
+    .catch(error => errorHandler(error));
+};
